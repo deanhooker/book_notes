5b158ce9aef338c951b25ff694481be1810b97cc
Dean Hooker <deanhooker91@gmail.com>
2018-10-01 21:38:13 -0600

    logout

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,7 @@ app_name = 'users'
 urlpatterns = [
     # Login page.
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Logout page.
+    path('logout/', views.logout_view, name='logout'),
 ]
