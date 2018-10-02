223494b919057b3250aa10acbe6514e688d1e30d
Dean Hooker <deanhooker91@gmail.com>
2018-10-01 21:59:25 -0600

    user registration

--- users/views.py
+++ users/views.py
@@ -1,9 +1,31 @@
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def logout_view(request):
     """Log the user out."""
     logout(request)
     return HttpResponseRedirect(reverse('notes:index'))
+
+def register(request):
+    """Register a new user."""
+    if request.method != 'POST':
+        # Display empty registration form.
+        form = UserCreationForm()
+    else:
+        # Process completed form.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Log the user in, then redirect to home page.
+            authenticated_user = authenticate(username=new_user.username,
+                password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('notes:index'))
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
